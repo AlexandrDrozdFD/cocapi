a3f4d71994845ad7e3c621e6d7ea99b654fae83f
Alexandr Drozd <alex.drozd.fd@gmail.com>
2022-03-11 12:28:31 +0300

    feat: add about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './pages/About';
+import About from './pages/About/About';
 import Home from './pages/Home';
 import Error from './pages/Error';
 import SingleCoctail from './pages/SingleCoctail';
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Welcome to TheCocktailDB
+        An open, crowd-sourced database of drinks and cocktails from around the world.
+        We also offer a free JSON API for anyone wanting to use it.
+        If you like the site, please consider supporting us on Patreon by clicking the link below...
+      </p>
+    </section>
+  );
+}
+
+export default About;
