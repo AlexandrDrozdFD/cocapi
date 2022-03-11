e0e70879b974f911c300f74f1db718add431c86a
Alexandr Drozd <alex.drozd.fd@gmail.com>
2022-03-11 09:33:26 +0300

    feat: add routes according to react-router v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
 import Home from './pages/Home';
+import Error from './pages/Error';
+import SingleCoctail from './pages/SingleCoctail';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div>
-      app 
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cocktail/:id' element={<SingleCoctail />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
